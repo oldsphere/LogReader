dc371a1df8d779fc91b2083d79d24585bcd242f6
crubio <crubio@nordsim.es>
2024-05-10 11:33:55 +0200

    Modified setup.py

--- setup.py
+++ setup.py
@@ -7,12 +7,13 @@ setup(
     author_email="crubio@nordsim.es",
     description="Lector de logs para OpenFOAM, generalizable a otros, basado en estructura de pyFoam",
     url="https://github.com/oldsphere/LogReader",
-    packages=find_packages(),
+    packages=find_packages(exclude=["test*"]),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.10",
+    python_requires=">=3.8",
+    install_requires=["PyFoam"],
     scripts=[],
 )
